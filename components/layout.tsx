929d1a0f6c50a1c00214d46ccb638a2fb66ed915
Zachary <zachgof64@protonmail.com>
2021-06-14 18:11:10 -0700

    Updated HomePage

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
-import NavBar from './navbar';
 import Head from 'next/head';
-import NavLink from './navlink';
-import styles from '../styles/layout.module.scss';
 
 
 interface LayoutProps {
@@ -18,16 +15,6 @@ class Layout extends Component<LayoutProps> {
             <meta name="description" content="A Jr. Developer's portfolio."/>
             <title>Zach G. - Folio</title>
           </Head>
-          <header className={styles.header}>
-            <NavBar>
-              {/* <NavLink href="#about">
-                About
-              </NavLink> */}
-              <NavLink href="/contact">
-                Contact
-              </NavLink>
-            </NavBar>
-          </header>
           <main>
             {this.props.children}
           </main>
