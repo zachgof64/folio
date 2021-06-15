92501b9a3947382ce1d4372cfffc63d88f8769ec
Zachary <zachgof64@protonmail.com>
2021-06-14 18:20:55 -0700

    removed docs

--- docs/_next/static/UZMTNBM5xtp2Vh9mARpE6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9a72ecc00c9fca6185f8.css","static/chunks/pages/index-c90ff7b4e979e4c96613.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
